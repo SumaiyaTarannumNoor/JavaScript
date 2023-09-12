d990b83589ba402659d6e7ed902aacdf64123aa4
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2023-09-12 17:59:38 +0600

    Update RockPaperScissor.js

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -1,15 +1,16 @@
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")() //comment out this line while using chrome dev-tools.
 
 let playerWins = 0;
 let computerWins = 0
 let ties = 0;
 
 let tries = 0;
+console.log("You have 6 tries. Let's Start...")
+while(tries <= 6){
+        
+        const playerChoice = prompt("Enter Rock/Paper/Scissors: ")
 
-while(tries < 6){
-        const playerChoice = prompt("Enter Rock/paper/Scissors: ")
-
-        if(playerChoice.toUpperCase() == "Rock" || "PAPER" || "SCISSORS"){
+        if(playerChoice.toUpperCase() === "ROCK" || playerChoice.toUpperCase() ==="PAPER" || playerChoice.toUpperCase() ==="SCISSORS"){
              console.log(playerChoice.toUpperCase())
              tries++;
         }
@@ -25,7 +26,7 @@ while(tries < 6){
 
         console.log("The computer chose: "+ computerChoice)
 
-        if (computerChoice == playerChoice){
+        if (computerChoice === playerChoice){
                 ties++
                 console.log("DRAW GAME! SCORE: " + ties)
         }
@@ -50,10 +51,10 @@ while(tries < 6){
 }
 
 if(playerWins>computerWins){
-        console.log("Player is the winner with " + playerWins + " score.")
+        console.log("Player is the winner with " + (playerWins-ties) + " score.")
 }                
 else{
-        console.log("Computer is the winner with " + computerWins + " score." )
+        console.log("Computer is the winner with " + (computerWins-ties)+ " score." )
 }      
                      
      
