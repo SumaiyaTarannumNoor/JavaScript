8ecf8f7aff344b0f467ad4e0209deffc99a876cd
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2023-09-12 11:50:54 +0600

    Add files via upload

---
+++ RandomNumberGuesser.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")()
+
+const target = (Math.round(Math.random())*20)
+
+
+let guesses = 0;
+
+while(guesses<16){
+    guesses++ ;
+    const guess = Number(prompt("Enter your guess: "))
+
+    if(guess>target){
+        console.log("Your guess is too high.")
+    }
+    else if(guess<target){
+        console.log("Your guess is too low.")
+    }
+    else{
+        console.log("Correct Guess")
+    break;
+    }
+}
+console.log("You guesses it in "+ guesses + " tries, GOOD!!")
