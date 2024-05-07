fc47d36c76e0a57802cd3a0d3ffee3b97de632b6
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-05-07 12:33:47 +0600

    Add files via upload

---
+++ Sabic-HackerRankLeaderBorad-Scrapper/Sabic-HackerRankLeaderBorad-Scrapper-snippetAdded.js
@@ -0,0 +1,53 @@
+
+/////// The original owner of this code is MI Sabic - https://github.com/MISabic
+/////// Original Code Repository - https://github.com/MISabic/hackerrank-leaderboard-scraper
+/////// I have no involvement with the code.
+/////// A small snippet is fixed that is probably due to an async-await issue. 
+
+
+const getLimit = (numberOfPages) => {
+    const range = numberOfPages * 10;
+    return ((range / 100) * 100 === range) ? range : range + 100;
+}
+
+const generateRankList = async (contestCode, numberOfPages) => {
+    let leaders = [];
+    let limit = getLimit(numberOfPages);
+
+    for(let i=0; i<=limit; i+=100) {
+        await fetch(`https://www.hackerrank.com/rest/contests/${contestCode}/leaderboard?offset=${i}&limit=100`)
+            .then(resp => resp.json())
+            .then(data => {
+                data.models.map(hacker => {
+                    const leader = {
+                        Rank: hacker?.rank,
+                        Hacker: hacker?.hacker,
+                        HackerID: hacker?.hacker_id,
+                        Country: hacker?.country,
+                        Problems: hacker?.challenges?.length,
+                        Solved: hacker?.solved_challenges,
+                        Score: hacker?.score,
+                        Time: hacker?.time_taken
+                    }
+                    leaders.push(leader);
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
+    return leaders;
+}
+
+const contestCode = "101hack52";
+const numberOfPages = 224;
+
+///////////Fixed Portion/////////
+
+async function run() {
+    const rankList = await generateRankList(contestCode, numberOfPages);
+    console.log(rankList);
+}
+
+run();
+
+///////////Fixed Portion/////////
