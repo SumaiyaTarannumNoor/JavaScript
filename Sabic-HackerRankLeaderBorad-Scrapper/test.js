6ba944813429f82a6c4bf54a9fa555148b0f1c51
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-05-07 12:25:53 +0600

    Add files via upload

--- Sabic-HackerRankLeaderBorad-Scrapper/test.js
+++ Sabic-HackerRankLeaderBorad-Scrapper/test.js
@@ -2,7 +2,7 @@
 /////// The original owner of this code is MI Sabic - https://github.com/MISabic
 /////// Original Code Repository - https://github.com/MISabic/hackerrank-leaderboard-scraper
 /////// I have no involvement with the code.
-/////// I have just fixed a small snippet that is probably due to an async-await issue using Chat-GPT. 😂😂
+/////// A small snippet is fixed that is probably due to an async-await issue. 
 
 
 const getLimit = (numberOfPages) => {
