dee87bc64120757f028596b7592b6cc6de028fdd
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-05-09 14:48:53 +0600

    Add files via upload

---
+++ Dom Manipulation - Full Course/4. Event Listners/app.js
@@ -0,0 +1,56 @@
+// DOM Manipulation
+
+// Event Listners
+
+// // comment out as you go *
+// // element.addEventListner("click", function);
+
+// const buttonTwo = document.querySelector('.btn-2');
+
+// function alertBtn(){
+//     alert('Yes, it is...');
+// }
+
+// buttonTwo.addEventListener('click', alertBtn)
+
+
+// // comment out as you go *
+// // Mouseover
+
+// const newBackgroundColor = document.querySelector('.box-3');
+
+// function changeBgColor(){
+//     newBackgroundColor.style.backgroundColor = "blue"
+// };
+
+// newBackgroundColor.addEventListener("mouseover", changeBgColor);
+
+
+////////////For this potion, Comment out the previous portion///////////
+
+// // comment out as you go *
+// // To remover the hover effect
+// const newBackgroundColor = document.querySelector('.box-3');
+// let defaultBgColor;
+
+// // Function to change background color on mouseover
+// function changeBgColor() {
+//     // Store the default background color if not already stored
+//     if (!defaultBgColor) {
+//         defaultBgColor = newBackgroundColor.style.backgroundColor;
+//     }
+//     // Change the background color to blue
+//     newBackgroundColor.style.backgroundColor = "blue";
+// };
+
+// // Function to revert back to default background color on mouseout
+// function revertBgColor() {
+//     // Revert back to default background color
+//     newBackgroundColor.style.backgroundColor = defaultBgColor;
+// };
+
+// // Event listener for mouseover
+// newBackgroundColor.addEventListener("mouseover", changeBgColor);
+
+// // Event listener for mouseout
+// newBackgroundColor.addEventListener("mouseout", revertBgColor);
