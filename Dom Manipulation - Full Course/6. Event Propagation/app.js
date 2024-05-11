d3662d4a86f4066bcd0297a90f4a4cb552792996
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-05-11 14:17:59 +0600

    Add files via upload

---
+++ Dom Manipulation - Full Course/6. Event Propagation/app.js
@@ -0,0 +1,146 @@
+// DOM Manipulation
+
+// Event Propagation
+// // comment out as you go *
+// window.addEventListener('click', function(){
+//     console.log('Window');
+// }, true);
+
+// document.addEventListener('click', function(){
+//     console.log('Document');
+// }, true);
+
+// document.querySelector(".div2").addEventListener('click', function(){
+//     console.log('DIV 2');
+// }, true);
+
+// document.querySelector(".div1").addEventListener('click', function(){
+//     console.log('DIV 1');
+// }, true);
+
+// document.querySelector("button").addEventListener('click', function(e){
+//     this.classList.add("enlarged");
+//     console.log(e.target.innerText = "Clicked!");
+// }, true);
+
+/////////////////////////////////////////
+// // comment out as you go *
+// window.addEventListener('click', function(){
+//     console.log('Window');
+// }, false);
+
+// document.addEventListener('click', function(){
+//     console.log('Document');
+// }, false);
+
+// document.querySelector(".div2").addEventListener('click', function(){
+//     console.log('DIV 2');
+// }, false);
+
+// document.querySelector(".div1").addEventListener('click', function(){
+//     console.log('DIV 1');
+// }, false);
+
+// document.querySelector("button").addEventListener('click', function(e){
+//     this.classList.add("enlarged");
+//     console.log(e.target.innerText = "Clicked!");
+// }, false);
+
+/////////////////////////////////////////////////////
+// // comment out as you go *
+// window.addEventListener('click', function(){
+//     console.log('Window');
+// }, false);
+
+// document.addEventListener('click', function(){
+//     console.log('Document');
+// }, false);
+
+// document.querySelector(".div2").addEventListener('click', function(e){
+//     e.stopPropagation()
+//     console.log('DIV 2');
+// }, false);
+
+// document.querySelector(".div1").addEventListener('click', function(){
+//     console.log('DIV 1');
+// }, false);
+
+// document.querySelector("button").addEventListener('click', function(e){
+//     this.classList.add("enlarged");
+//     console.log(e.target.innerText = "Clicked!");
+// }, false);
+
+
+/////////////////////////////////////////////////////
+// // comment out as you go *
+// window.addEventListener('click', function(){
+//     console.log('Window');
+// }, true);
+
+// document.addEventListener('click', function(){
+//     console.log('Document');
+// }, true);
+
+// document.querySelector(".div2").addEventListener('click', function(e){
+//     e.stopPropagation()
+//     console.log('DIV 2');
+// }, true);
+
+// document.querySelector(".div1").addEventListener('click', function(){
+//     console.log('DIV 1');
+// }, true);
+
+// document.querySelector("button").addEventListener('click', function(e){
+//     this.classList.add("enlarged");
+//     console.log(e.target.innerText = "Clicked!");
+// }, true);
+
+//////////////////////////////////////////////////////////
+// // comment out as you go *
+// window.addEventListener('click', function(){
+//     console.log('Window');
+// }, false);
+
+// document.addEventListener('click', function(){
+//     console.log('Document');
+// }, false);
+
+// document.querySelector(".div2").addEventListener('click', function(){
+//     console.log('DIV 2');
+// }, {once: true});
+
+// document.querySelector(".div1").addEventListener('click', function(){
+//     console.log('DIV 1');
+// }, false);
+
+// document.querySelector("button").addEventListener('click', function(e){
+//     this.classList.add("enlarged");
+//     console.log(e.target.innerText = "Clicked!");
+// }, false);
+
+
+//////////// For index2.html ///////////////////
+////////////////////////////////////////////////
+
+//comment out as you go *
+window.addEventListener('click', function(){
+    console.log('Window');
+}, false);
+
+document.addEventListener('click', function(){
+    console.log('Document');
+}, false);
+
+document.querySelector(".div2").addEventListener('click', function(){
+    console.log('DIV 2');
+}, {once: true});
+
+document.querySelector(".div1").addEventListener('click', function(){
+    console.log('DIV 1');
+}, false);
+
+document.querySelector(".button").addEventListener('click', function(e){
+    this.classList.add("enlarged");
+    e.preventDefault();
+    console.log(e.target.innerText = "Clicked!");
+}, false);
